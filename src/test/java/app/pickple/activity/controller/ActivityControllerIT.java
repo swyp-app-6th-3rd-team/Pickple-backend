899129d68d77ced99b6780cf36bc7a6747972546
habin <danny9643@naver.com>
2026-09-04 22:45:38 +0900

    style: 테스트 import 순서 정리

--- src/test/java/app/pickple/activity/controller/ActivityControllerIT.java
+++ src/test/java/app/pickple/activity/controller/ActivityControllerIT.java
@@ -26,9 +26,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.bean.override.convention.TestBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.context.bean.override.convention.TestBean;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
